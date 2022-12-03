5d9e87b1501a5e6193391562dc99befa1fe5e0cb
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2022-12-03 13:45:04 +0530

    Time: 23 ms (38.97%), Space: 10.1 MB (57.79%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    int minPathSumUtil(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
-        if(i==0 && j==0)
-            return grid[i][j];
-        if(i<0 || j<0)
-            return 1e9;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int up=grid[i][j]+minPathSumUtil(i-1,j,grid,dp);
-        int left=grid[i][j]+minPathSumUtil(i,j-1,grid,dp);
-        return dp[i][j]=min(up,left);
+    int minPathSum(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+      vector<vector<int> > dp(m,vector<int>(n,0));
+    for(int i=0; i<m ; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 && j==0) dp[i][j] = matrix[i][j];
+            else{
+                
+                int up = matrix[i][j];
+                if(i>0) up += dp[i-1][j];
+                else up += 1e9;
+                
+                int left = matrix[i][j];
+                if(j>0) left+=dp[i][j-1];
+                else left += 1e9;
+                
+                dp[i][j] = min(up,left);
+            }
+        }
     }
-    int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-       return minPathSumUtil(m-1,n-1,grid,dp);
+    
+    return dp[m-1][n-1];
+    
     }
 };
