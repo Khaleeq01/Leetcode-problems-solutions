f0cdaf89810f17cc82d45e80143b150fac33295b
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2022-12-04 19:46:38 +0530

    Time: 13 ms (51.23%), Space: 9.1 MB (10.01%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int minPathSum(int i,int j,vector<vector<int>>&triangle,int n,vector<vector<int>>&dp){
-        if(i==n-1)
-            return triangle[i][j];
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int down=triangle[i][j]+minPathSum(i+1,j,triangle,n,dp);
-        int diagonal=triangle[i][j]+minPathSum(i+1,j+1,triangle,n,dp);
-        return dp[i][j]=min(down,diagonal);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return minPathSum(0,0,triangle,n,dp);
+          int n=triangle.size();
+         vector<vector<int> > dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++){
+        dp[n-1][j] = triangle[n-1][j];
+         }
+      for(int i=n-2; i>=0; i--){
+        for(int j=i; j>=0; j--){
+            int down = triangle[i][j]+dp[i+1][j];
+            int diagonal = triangle[i][j]+dp[i+1][j+1];
+            dp[i][j] = min(down, diagonal);
+        }
+    }
+    return dp[0][0];
     }
 };
