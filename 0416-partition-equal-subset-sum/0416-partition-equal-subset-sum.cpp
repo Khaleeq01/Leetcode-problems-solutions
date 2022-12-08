afa091850c7bdc4b444562463067ca194c53f15b
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2022-12-08 12:36:13 +0530

    Time: 298 ms (68.07%), Space: 9.7 MB (85.56%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    bool subsetSum(int ind,int target,vector<int>& nums,vector<vector<int>> &dp){
-        if(target==0)
-            return true;
-        if(ind==0)
-           return nums[0]==target;
-        if(dp[ind][target]!=-1)
-            return dp[ind][target];
-        bool notTaken=subsetSum(ind-1,target,nums,dp);
-        bool taken=false;
-        if(nums[ind]<=target){
-            taken=subsetSum(ind-1,target-nums[ind],nums,dp);
-        }
-        return  dp[ind][target]=notTaken||taken;
-    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int totalSum=0;
@@ -24,8 +10,21 @@ public:
             return false;
         else{
             int k=totalSum/2;
-            vector<vector<int>> dp(n,vector<int>(k+1,-1));
-            return subsetSum(n-1,k,nums,dp);
+            vector<bool>prev(k+1,false),cur(k+1,false);
+                prev[0]=cur[0]=true;
+            if(nums[0]<=k)
+                prev[nums[0]]=true;
+            for(int ind=1;ind<n;ind++){
+                for(int target=1;target<=k;target++){
+                    bool notTaken=prev[target];
+                    bool taken=false;
+                    if(nums[ind]<=target)
+                        taken=prev[target-nums[ind]];
+                    cur[target]=notTaken||taken;
+                }
+                prev=cur;
+            }
+            return prev[k];
         }
     }
 };
