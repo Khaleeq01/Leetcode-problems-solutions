e1b34d479e4b01abd7417e5af1bcea8a18e46fa8
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2022-12-15 13:06:58 +0530

    Time: 31 ms (61.80%), Space: 7.3 MB (76.01%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<int>prev(amount+1,0),cur(amount+1,0);
         for(int T=0;T<=amount;T++){
-                dp[0][T]=(T%coins[0]==0);
+                prev[T]=(T%coins[0]==0);
         }
         for(int ind=1;ind<n;ind++){
             for(int T=0;T<=amount;T++){
-                 int notTake=dp[ind-1][T];
+                 int notTake=prev[T];
            int take=0;
           if(coins[ind]<=T)
-         take=dp[ind][T-coins[ind]];
-     dp[ind][T]=(notTake+take);
+         take=cur[T-coins[ind]];
+     cur[T]=(notTake+take);
             }
+            prev=cur;
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 };
