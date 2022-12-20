f92a2840d3da55c15ea9eb9f17d559d0f3fb463f
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2022-12-20 11:57:18 +0530

    Time: 144 ms (50.04%), Space: 33 MB (37.44%) - LeetHub

---
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int similarPairs(vector<string>& words) {
+        int cnt=0;
+        vector<set<char>>v;
+        for(int i=0;i<words.size();i++){
+            set<char>st(words[i].begin(),words[i].end());
+            v.push_back(st);
+        }
+        for(int i=0;i<words.size()-1;i++){
+            for(int j=i+1;j<words.size();j++){
+                if(v[i]==v[j])
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+};
