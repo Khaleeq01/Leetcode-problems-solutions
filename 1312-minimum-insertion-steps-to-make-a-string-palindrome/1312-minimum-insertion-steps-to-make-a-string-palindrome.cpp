c196dc8a063aec0b0eb9f9ed9b0c552bf8af7ac0
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2022-12-23 16:27:38 +0530

    Time: 101 ms (53.92%), Space: 6.8 MB (90.43%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -3,16 +3,17 @@ public:
     int lcs(string s1,string s2){
         int n=s1.size();
         int m=s2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        vector<int>prev(m+1,0),cur(m+1,0);
         for(int ind1=1;ind1<=n;ind1++){
             for(int ind2=1;ind2<=m;ind2++){
                 if(s1[ind1-1]==s2[ind2-1])
-                    dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+                    cur[ind2]=1+prev[ind2-1];
                 else
-                    dp[ind1][ind2]=0+max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                    cur[ind2]=0+max(prev[ind2],cur[ind2-1]);
             }
+            prev=cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
     int longestPalindromicSubsequence(string s){
         string t=s;
