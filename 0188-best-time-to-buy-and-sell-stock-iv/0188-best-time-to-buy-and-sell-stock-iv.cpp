2243bded5f9a42934653974b590beaff36fa6bbc
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-01-03 16:50:35 +0530

    Time: 32 ms (41.74%), Space: 14 MB (42.19%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    int solve(int index,int operationNo,int k,vector<int>& prices,vector<vector<int>>& dp){
-        if(index==prices.size())
-            return 0;
-        if(operationNo==2*k)
-            return 0;
-        if(dp[index][operationNo]!=-1)
-            return dp[index][operationNo];
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(2*k+1,0));
         int profit=0;
-        if(operationNo%2==0){
+        for(int index=n-1;index>=0;index--){
+            for(int operationNo=0;operationNo<2*k;operationNo++){
+                if(operationNo%2==0){
             //buy is allowed
-            int buy=-prices[index]+solve(index+1,operationNo+1,k,prices,dp);
-            int skip=0+solve(index+1,operationNo,k,prices,dp);
+            int buy=-prices[index]+dp[index+1][operationNo+1];
+            int skip=0+dp[index+1][operationNo];
             profit=max(buy,skip);
         }
         else{
-            int sell=prices[index]+solve(index+1,operationNo+1,k,prices,dp);
-            int skip=0+solve(index+1,operationNo,k,prices,dp);
+            int sell=prices[index]+dp[index+1][operationNo+1];
+            int skip=0+dp[index+1][operationNo];
             profit=max(sell,skip);
-        }
-        return dp[index][operationNo]= profit;
-    }
-    int maxProfit(int k, vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2*k,-1));
-      return solve(0,0,k,prices,dp);
+           }
+         dp[index][operationNo]= profit;
+             }
+            }
+      return dp[0][0];
     }
 };
