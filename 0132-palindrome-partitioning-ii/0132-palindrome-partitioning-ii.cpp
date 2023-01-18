cd8203a0fc9bf3d90a6953ad2dcf6ba391f3b1d6
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-01-18 11:43:47 +0530

    Time: 414 ms (42.62%), Space: 6.5 MB (75.19%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     bool isPalindrome(int i,int j,string& s){
-        while(i<=j){
-            if(s[i++]!=s[j--])
+        while(i<j){
+            if(s[i]!=s[j])
                 return false;
-           
+            i++;
+            j--;
         }
         return true;
     }
-    
-    int minCut(string str) {
+    int f(int i,int j,string& str,vector<int>& dp){
         int n=str.size();
-        vector<int>dp(n+1,0);
-        dp[n]=0;
-        for(int i=n-1;i>=0;i--){
-             int minCost=INT_MAX;
+        if(i==n)
+            return 0;
+        if(dp[i]!=-1)
+            return dp[i];
+        int minCost=INT_MAX;
         for(int j=i;j<n;j++){
             if(isPalindrome(i,j,str)){
-               int cost=1+dp[j+1];
+               int cost=1+f(j+1,n,str,dp);
                 minCost=min(minCost,cost);
             }
         }
-     dp[i]=minCost;
-        }
-        return dp[0]-1;
+        return dp[i]=minCost;
+    }
+    int minCut(string str) {
+        int n=str.size();
+        vector<int>dp(n,-1);
+        return f(0,n,str,dp)-1;
     }
 };
