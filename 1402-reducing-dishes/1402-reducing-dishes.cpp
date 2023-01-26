722eb0cfbac2ddf7273b4b193b327a1b5f6d5792
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-01-26 12:30:17 +0530

    Time: 48 ms (36.85%), Space: 8.2 MB (68.22%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -3,14 +3,15 @@ public:
     int maxSatisfaction(vector<int>& satisfaction) {
         int n=satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end());
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+       vector<int>next(n+1,0),curr(n+1,0);
         for(int index=n-1;index>=0;index--){
             for(int time=index;time>=0;time--){
-                  int include=satisfaction[index]*(time+1)+dp[index+1][time+1];
-        int exclude=0+dp[index+1][time];
-         dp[index][time]= max(include,exclude);
+                  int include=satisfaction[index]*(time+1)+next[time+1];
+        int exclude=0+next[time];
+         curr[time]= max(include,exclude);
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
