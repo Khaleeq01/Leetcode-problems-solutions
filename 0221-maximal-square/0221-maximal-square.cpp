39f4832f6462e2629a976e2c9103d3f121039d51
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-02-13 13:29:12 +0530

    Time: 81 ms (94.63%), Space: 18.7 MB (91.98%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -4,20 +4,21 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
         int maxi=0;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+      vector<int>curr(m+1,0),next(m+1,0);
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
-        int right = dp[i][j+1];
-        int diagonal = dp[i+1][j+1];
-        int down = dp[i+1][j];
+        int right = curr[j+1];
+        int diagonal = next[j+1];
+        int down = next[j];
         if(matrix[i][j] == '1'){
             int ans = 1 + min(right, min(diagonal, down));
             maxi = max(maxi, ans);
-            dp[i][j]=ans;
+            curr[j]=ans;
         }
         else
-             dp[i][j]= 0;
+             curr[j]= 0;
             }
+            next=curr;
         }
         return maxi*maxi;
     }
