8b252f93cf40e728446d06d7e88afa0fa1f0f696
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-03-05 13:38:22 +0530

    Time: 3 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        if(time<n){
+            return 1+time;
+        }
+        int rem=time%(n-1);
+        int quo=time/(n-1);
+        if(quo%2!=0) {
+             return (n-rem);
+        }
+            return rem+1;
+    }
+};
