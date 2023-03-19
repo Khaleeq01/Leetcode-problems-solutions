a56c73044887dc59f16479fc81a1bfe96cfd698c
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-03-19 13:17:21 +0530

    Time: 3 ms (100.00%), Space: 7.3 MB (100.00%) - LeetHub

---
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> evenOddBit(int n) {
+        int i=0,even=0,odd=0;
+        while(n){
+            if(n%2){
+                if(i%2)
+                    odd++;
+                else
+                    even++;
+            }
+            n=n/2;
+            i++;
+        }
+        vector<int>ans;
+        ans.push_back(even);
+        ans.push_back(odd);
+        return ans;
+    }
+};
