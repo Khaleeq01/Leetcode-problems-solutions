5236ea542e1a3efa49cad64a92ab98fb3f7072b5
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-04-16 14:27:03 +0530

    Time: 5 ms (60.47%), Space: 9.8 MB (37.02%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,6 +4,8 @@ public:
     int s=0,e=nums.size()-1;
         while(s<=e){
             int m=s+(e-s)/2;
+            if(nums[m]==target)
+                return m;
             if(nums[m]<target)
                 s=m+1;
             else
