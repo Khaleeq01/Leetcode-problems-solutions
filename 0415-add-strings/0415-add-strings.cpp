ae925a8fd331a55064cb157addd77fb50fe4cad4
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-05-27 12:57:52 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (87.20%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i=num1.size()-1,j=num2.size()-1;
+        int carry=0;
+        string ans="";
+        while(i>=0 || j>=0 || carry>0){
+            long sum=0;
+            if(i>=0){
+                sum+=num1[i]-'0';
+                i--;
+            }
+            if(j>=0){
+                sum+=num2[j]-'0';
+                j--;
+            }
+            sum+=carry;
+            carry=sum/10;
+            ans+=to_string(sum%10);
+        }
+         reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
