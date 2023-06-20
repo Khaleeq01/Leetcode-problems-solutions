a839a2dcb72d4737db57eee1c0e0f3d64bab5737
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-06-20 17:07:35 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -10,40 +10,30 @@ using namespace std;
 
 class Solution {
   public:
+  void dfs(int i,int j,int n, int m,vector<vector<int>> &grid){
+      if(i<0 || i>=n || j<0 || j>=m || grid[i][j]!=1){
+          return;
+      }
+      grid[i][j]=0;
+      dfs(i+1,j,n,m,grid);
+      dfs(i-1,j,n,m,grid);
+      dfs(i,j+1,n,m,grid);
+      dfs(i,j-1,n,m,grid);
+  }
     int numberOfEnclaves(vector<vector<int>> &grid) {
-         queue<pair<int,int>>q;
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(i==0 || i==n-1 || j==0 || j==m-1){
-                if(grid[i][j]==1){
-                    q.push({i,j});
-                    vis[i][j]=1;
-                }
-            }
-        }
-        }
-        int del_row[]={-1,0,1,0};
-        int del_col[]={0,1,0,-1};
-        while(!q.empty()){
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nrow=row+del_row[i];
-                int ncol=col+del_col[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                q.push({nrow,ncol});
-                vis[nrow][ncol]=1;
+                    dfs(i,j,n,m,grid);
                 }
             }
         }
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && vis[i][j]==0)
+                if(grid[i][j]==1)
                 count++;
             }
         }
