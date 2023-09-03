0e3e088d7ba98d147844771735e15e85f6253d89
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-09-03 15:36:15 +0530

    Time: 164 ms (100.00%), Space: 6.1 MB (60.00%) - LeetHub

---
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int ans=0;
+        for(int i=low;i<=high;i++){
+            string s=to_string(i);
+            int n=s.size();
+            int sum=0;
+            for(int j=0;j<n/2;j++){
+                sum+=s[j]-s[n-j-1];
+            }
+            if(n%2==0 && sum==0) ans++;
+        }
+        return ans;
+    }
+};
