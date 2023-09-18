048631f14f4d58ac7b894e12276b7f6291160dbe
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-09-18 16:11:06 +0530

    Time: 99 ms (40.00%), Space: 72.1 MB (20.00%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2860-happy-students/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2860-happy-students.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countWays(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int ans=0;
+         if(nums[0]>0) ans++;
+        for(int i=0;i<n-1;i++){
+            if( (i>=nums[i]) && (i+1<nums[i+1])) ans++;
+        }
+        if(n-1>=nums[n-1]) ans++;
+        return ans;
+    }
+};
