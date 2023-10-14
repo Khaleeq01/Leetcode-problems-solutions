3d714d3de318494aa467efbe3db73d54f0e58b28
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-10-14 14:01:38 +0530

    Time: 153 ms (63.81%), Space: 90.1 MB (75.98%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2742-painting-the-walls/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2742-painting-the-walls.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int dp[501][501]={};
+    int solve(vector<int>& cost, vector<int>& time,int i,int wallRemaining){
+        //base case
+        if(wallRemaining<=0) return 0;
+        if(i>=cost.size()) return 1000000000;
+        if(dp[i][wallRemaining]!=0) return dp[i][wallRemaining];
+        int notTake=solve(cost,time,i+1,wallRemaining);
+        int take=cost[i]+solve(cost,time,i+1,wallRemaining-time[i]-1);
+        return dp[i][wallRemaining]=min(notTake,take);
+    }
+    int paintWalls(vector<int>& cost, vector<int>& time) {
+        return solve(cost,time,0,time.size());
+    }
+};
