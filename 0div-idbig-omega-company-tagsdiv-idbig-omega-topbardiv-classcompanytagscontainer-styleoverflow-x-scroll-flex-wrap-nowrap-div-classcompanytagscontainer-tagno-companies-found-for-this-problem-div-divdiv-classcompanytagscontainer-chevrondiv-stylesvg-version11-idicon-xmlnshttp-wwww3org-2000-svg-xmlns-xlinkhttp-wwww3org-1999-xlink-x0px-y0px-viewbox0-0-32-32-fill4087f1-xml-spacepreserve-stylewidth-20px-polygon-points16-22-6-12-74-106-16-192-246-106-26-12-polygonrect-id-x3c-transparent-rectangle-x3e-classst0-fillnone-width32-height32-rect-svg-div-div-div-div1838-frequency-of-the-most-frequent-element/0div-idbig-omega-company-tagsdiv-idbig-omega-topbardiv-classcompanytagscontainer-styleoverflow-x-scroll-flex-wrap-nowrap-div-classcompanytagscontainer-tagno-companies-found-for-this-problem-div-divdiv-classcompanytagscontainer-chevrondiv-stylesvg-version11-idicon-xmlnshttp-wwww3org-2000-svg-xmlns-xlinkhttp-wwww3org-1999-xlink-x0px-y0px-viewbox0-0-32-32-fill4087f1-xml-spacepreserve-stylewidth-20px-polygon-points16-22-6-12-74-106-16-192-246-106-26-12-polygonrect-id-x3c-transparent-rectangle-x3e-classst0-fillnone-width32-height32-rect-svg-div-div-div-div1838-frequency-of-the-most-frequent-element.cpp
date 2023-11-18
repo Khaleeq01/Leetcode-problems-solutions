37d1adec66dd161871050c9b7fb7d39501db3d58
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-11-18 19:56:19 +0530

    Time: 163 ms (79.14%), Space: 99.5 MB (25.44%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondiv-stylesvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1838-frequency-of-the-most-frequent-element/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondiv-stylesvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1838-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        long l=0,r=0,n=nums.size(),ans=0;
+        long long sum=0;
+        sort(nums.begin(),nums.end());
+        for(;r<n;r++){
+            sum+=nums[r];
+        for(;nums[r]*(r-l+1)-sum>k;l++){
+            sum-=nums[l];
+        }
+        ans=max(ans,r-l+1);
+        }
+        return ans;
+    }
+};
