6e8a8ed3aa3a804701ab7b3af88cb778b05ba98c
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-11-20 13:01:51 +0530

    Time: 12 ms (14.29%), Space: 7.3 MB (71.43%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2937-make-three-strings-equal/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2937-make-three-strings-equal.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int findMinimumOperations(string s1, string s2, string s3) {
+        int i=0, sz=min({s1.size(),s2.size(),s3.size()});
+        while(i<sz && s1[i]==s2[i] && s2[i]==s3[i]) i++;
+        return i==0?-1: s1.size()+s2.size()+s3.size()-3*i;
+    }
+};
