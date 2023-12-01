b061b277452526356fb4dda694654cc74226944b
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-12-01 19:56:09 +0530

    Added solution - LeetHub

--- Inorder Traversal - GFG/inorder-traversal.cpp
+++ Inorder Traversal - GFG/inorder-traversal.cpp
@@ -16,32 +16,19 @@ struct Node {
 
 
 // } Driver Code Ends
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-}; */
-
 class Solution {
   public:
-    void help(Node* root ,vector<int>& ans){
-        if(root==NULL)
-        return;
+    // Function to return a list containing the inorder traversal of the tree.
+   void help(Node* root,vector<int>& ans){
+        if(root==NULL) return;
         help(root->left,ans);
         ans.push_back(root->data);
         help(root->right,ans);
     }
     vector<int> inOrder(Node* root) {
-      vector<int>ans;
-      help(root,ans);
-      return ans;
+        vector<int>ans;
+        help(root,ans);
+        return ans;
     }
 };
 
