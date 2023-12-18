ca87fed10424d6d21da0e8f8400745e3b5615f2f
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-12-18 19:31:29 +0530

    Time: 21 ms (70.62%), Space: 20.6 MB (64.73%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1913-maximum-product-difference-between-two-pairs/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1913-maximum-product-difference-between-two-pairs.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1913-maximum-product-difference-between-two-pairs/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1913-maximum-product-difference-between-two-pairs.cpp
@@ -2,10 +2,28 @@ class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
         int n=nums.size();
-        sort(nums.begin(),nums.end());
+        int largest=INT_MIN;
+        int secondLargest=INT_MIN;
+        int smallest=INT_MAX;
+        int secondSmallest=INT_MAX;
         for(int i=0;i<n;i++){
-            return (nums[n-1]*nums[n-2])-(nums[0]*nums[1]);
+            //check for largest and secondLaargest
+            if(nums[i]>largest){
+                secondLargest=largest;
+                largest=nums[i];
+            }
+            else if(nums[i]>secondLargest){
+                secondLargest=nums[i];
+            }
+            //check for smallest and secondSmallest
+            if(nums[i]<smallest){
+                secondSmallest=smallest;
+                smallest=nums[i];
+            }
+            else if(nums[i]<secondSmallest){
+                secondSmallest=nums[i];
+            }
         }
-        return -1;
+        return (largest*secondLargest)-(smallest*secondSmallest);
     }
 };
