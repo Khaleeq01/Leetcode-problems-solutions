35e3859cd43a8bc8d7eee4859bc9887725ced651
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2023-12-20 22:49:22 +0530

    Time: 15 ms (45.23%), Space: 47 MB (58.30%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(),prices.end());
-        for(int i=0;i<prices.size();i++){
-            if(money>=prices[0]+prices[1])
-                return money-prices[0]-prices[1];
+        int firstMin=min(prices[0],prices[1]);
+        int secondMin=max(prices[0],prices[1]);
+        for(int i=2;i<prices.size();i++){
+            if(prices[i]<firstMin){
+                secondMin=firstMin;
+                firstMin=prices[i];
+            }
+            else if(prices[i]<secondMin){
+                secondMin=prices[i];
+            }
         }
+            if(money-(firstMin+secondMin)>=0)
+                return money-(firstMin+secondMin);
         return money;
     }
 };
