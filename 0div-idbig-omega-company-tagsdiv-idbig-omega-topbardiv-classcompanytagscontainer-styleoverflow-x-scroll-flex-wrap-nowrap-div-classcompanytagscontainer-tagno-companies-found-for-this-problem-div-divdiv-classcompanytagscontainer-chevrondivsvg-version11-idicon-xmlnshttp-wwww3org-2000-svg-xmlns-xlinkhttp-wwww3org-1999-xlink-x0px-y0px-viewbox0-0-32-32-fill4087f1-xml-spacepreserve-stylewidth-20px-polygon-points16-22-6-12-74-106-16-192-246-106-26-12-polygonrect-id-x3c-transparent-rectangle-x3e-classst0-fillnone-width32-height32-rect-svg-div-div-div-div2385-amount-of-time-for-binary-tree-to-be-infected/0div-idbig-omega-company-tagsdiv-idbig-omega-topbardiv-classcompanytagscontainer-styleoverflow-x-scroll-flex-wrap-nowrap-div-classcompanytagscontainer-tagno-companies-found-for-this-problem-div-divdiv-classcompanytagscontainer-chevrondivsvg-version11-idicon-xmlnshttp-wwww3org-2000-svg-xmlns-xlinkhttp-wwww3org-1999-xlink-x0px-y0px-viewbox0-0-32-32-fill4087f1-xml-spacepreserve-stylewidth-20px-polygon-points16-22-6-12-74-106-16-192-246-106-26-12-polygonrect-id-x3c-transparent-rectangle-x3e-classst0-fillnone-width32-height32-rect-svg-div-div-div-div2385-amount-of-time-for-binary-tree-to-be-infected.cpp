a519673324832233edb5d044f31382835af341a7
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-01-10 14:43:26 +0530

    Time: 569 ms (21.97%), Space: 181.6 MB (33.04%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2385-amount-of-time-for-binary-tree-to-be-infected/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,89 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+     TreeNode* parentMapping(TreeNode* root, int start, map<TreeNode*, TreeNode*> &nodeToParent){
+        queue<TreeNode*> q;
+        q.push(root);
+        nodeToParent[root] = NULL;
+         TreeNode* res = NULL;
+        
+        while(!q.empty()){
+            TreeNode* front = q.front();
+            q.pop();
+            
+            if (start == front->val)
+                 res = front;
+            
+            if(front->left){
+                q.push(front->left);
+                nodeToParent[front->left] = front;
+            }
+            
+            if(front->right){
+                q.push(front->right);
+                nodeToParent[front->right] = front;
+            }
+        }
+        return res;
+    }
+    
+    int burningTime(TreeNode*root, map<TreeNode*, TreeNode*>nodeToParent){
+        queue<TreeNode*> q;
+        map<TreeNode*, bool> visited;
+        int cnt = 0;
+        
+        q.push(root);
+        visited[root] = 1;
+        
+        while(!q.empty()){
+            bool flag = 0;
+            
+            int size = q.size();
+            for(int i = 0; i<size; i++){
+            TreeNode* front = q.front();
+                q.pop();
+            
+                if(front->left && !visited[front->left]){
+                    flag = 1;
+                    q.push(front->left);
+                    visited[front->left] = 1;
+                }
+                
+                if(front->right && !visited[front->right]){
+                    flag = 1;
+                    q.push(front->right);
+                    visited[front->right] = 1;
+                }
+                
+                if(nodeToParent[front] && !visited[nodeToParent[front]]){
+                    flag = 1;
+                    q.push(nodeToParent[front]);
+                    visited[nodeToParent[front]] = 1;
+                }
+                
+            }
+            if(flag == 1)
+             cnt++;
+        }
+        
+        return cnt;
+        
+    }
+public:
+    int amountOfTime(TreeNode* root, int start) {
+        map<TreeNode*, TreeNode*> nodeToParent;
+        TreeNode* startNode = parentMapping(root, start, nodeToParent);
+        int ans = burningTime(startNode, nodeToParent);
+        return ans;
+    }
+};
