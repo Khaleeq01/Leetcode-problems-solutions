f7bf7519d0d58060343e556c6f95a20c4033eb8d
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-01-15 16:20:50 +0530

    Time: 463 ms (75.73%), Space: 207.9 MB (7.64%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2225-find-players-with-zero-or-one-losses/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2225-find-players-with-zero-or-one-losses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        vector<int>lostCount(100001,-1);
+        for(auto match:matches){
+            int pWin=match[0],pLost=match[1];
+            if(lostCount[pWin]==-1) lostCount[pWin]=0;
+            if(lostCount[pLost]==-1) lostCount[pLost]=1;
+            else lostCount[pLost]++;
+        }
+        vector<int>zeroLost,oneLost;
+        for(int i=0;i<100001;i++){
+            if(lostCount[i]==0) zeroLost.push_back(i);
+            if(lostCount[i]==1) oneLost.push_back(i);
+        }
+        return {zeroLost,oneLost};
+    }
+};
