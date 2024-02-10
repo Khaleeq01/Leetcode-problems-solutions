171b8df3d839b1bf1a181b1c7f14ee304ec2fd4b
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-02-10 14:52:15 +0530

    Time: 4 ms (76.20%), Space: 7.6 MB (68.03%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence24-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitter-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divexpedia-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div647-palindromic-substrings/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence24-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitter-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divexpedia-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div647-palindromic-substrings.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence24-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitter-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divexpedia-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div647-palindromic-substrings/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence24-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtwitter-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divexpedia-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div647-palindromic-substrings.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int isPalindrome(string s,int i,int j,vector<vector<int>>& dp){
-       if(i>=j) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=s[i]==s[j]?isPalindrome(s,i+1,j-1,dp):0;
+    int palindromicCount(string& s, int i,int j){
+        int count=0;
+        while(i>=0 && j<s.size() && s[i]==s[j]){
+            i--;
+            j++;
+            count++;
+        }
+        return count;
     }
     int countSubstrings(string s) {
         int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
         int count=0;
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                count+=isPalindrome(s,i,j,dp);
-            }
+            int even=palindromicCount(s,i,i+1);
+            int odd=palindromicCount(s,i,i);
+            count+=even+odd;
         }
         return count;
     }
