59e8ab0f520dae8b4f118563aa21ce2d1f7eb273
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-04-20 15:56:08 +0530

    Time: 90 ms (89.45%), Space: 59.2 MB (97.82%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcitrix-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1992-find-all-groups-of-farmland/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcitrix-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1992-find-all-groups-of-farmland.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcitrix-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1992-find-all-groups-of-farmland/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcitrix-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1992-find-all-groups-of-farmland.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        int n=land.size(),m=land[0].size();
-        vector<vector<int>>ans;
-        vector<vector<bool>>visited(n,vector<bool>(m,false));
-        for(int r1=0;r1<n;r1++){
-            for(int c1=0;c1<m;c1++){
-                if(land[r1][c1] && !visited[r1][c1]){
-                    int r2=c1,c2=c1;
-                    for(r2=r1; r2<n && land[r2][c1];r2++){
-                        for(c2=c1; c2<m && land[r2][c2];c2++){
-                            visited[r2][c2]=true;
+        int n = land.size(), m = land[0].size();
+        vector<vector<int>> ans;
+        
+        for (int r1 = 0; r1 < n; r1++) {
+            for (int c1 = 0; c1 < m; c1++) {
+                if (land[r1][c1]) {
+                    int r2 = r1, c2 = c1;
+                    
+                    for (r2 = r1; r2 < n && land[r2][c1]; r2++) {
+                        for (c2 = c1; c2 < m && land[r2][c2]; c2++) {
+                            land[r2][c2] = 0;
                         }
                     }
-                    ans.push_back({r1,c1,r2-1,c2-1});
+
+                    ans.push_back({r1, c1, r2 - 1, c2 - 1});
                 }
             }
         }
+
         return ans;
     }
 };
