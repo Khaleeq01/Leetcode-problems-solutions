6fd08dc0899929a4488e02845c045368447e39fd
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-04-27 12:15:41 +0530

    Time: 123 ms (26.89%), Space: 78 MB (16.98%) - LeetHub

---
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(string ring,string key,int pt1,int pt2,vector<vector<int>>& dp){
+        int n=ring.size();
+        int m=key.size();
+        if(pt2>=m) return 0;
+        if(dp[pt1][pt2]!=-1) return dp[pt1][pt2];
+        int steps=INT_MAX;
+        for(int i=0;i<n;i++){
+            if(ring[i]==key[pt2]){
+                steps=min(steps,min(abs(i-pt1),n-abs(i-pt1))+1+solve(ring,key,i,pt2+1,dp));
+            }
+        }
+        return dp[pt1][pt2]=steps;
+    }
+    int findRotateSteps(string ring, string key) {
+        int n=ring.size();
+        int m=key.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return solve(ring,key,0,0,dp);
+    }
+};
