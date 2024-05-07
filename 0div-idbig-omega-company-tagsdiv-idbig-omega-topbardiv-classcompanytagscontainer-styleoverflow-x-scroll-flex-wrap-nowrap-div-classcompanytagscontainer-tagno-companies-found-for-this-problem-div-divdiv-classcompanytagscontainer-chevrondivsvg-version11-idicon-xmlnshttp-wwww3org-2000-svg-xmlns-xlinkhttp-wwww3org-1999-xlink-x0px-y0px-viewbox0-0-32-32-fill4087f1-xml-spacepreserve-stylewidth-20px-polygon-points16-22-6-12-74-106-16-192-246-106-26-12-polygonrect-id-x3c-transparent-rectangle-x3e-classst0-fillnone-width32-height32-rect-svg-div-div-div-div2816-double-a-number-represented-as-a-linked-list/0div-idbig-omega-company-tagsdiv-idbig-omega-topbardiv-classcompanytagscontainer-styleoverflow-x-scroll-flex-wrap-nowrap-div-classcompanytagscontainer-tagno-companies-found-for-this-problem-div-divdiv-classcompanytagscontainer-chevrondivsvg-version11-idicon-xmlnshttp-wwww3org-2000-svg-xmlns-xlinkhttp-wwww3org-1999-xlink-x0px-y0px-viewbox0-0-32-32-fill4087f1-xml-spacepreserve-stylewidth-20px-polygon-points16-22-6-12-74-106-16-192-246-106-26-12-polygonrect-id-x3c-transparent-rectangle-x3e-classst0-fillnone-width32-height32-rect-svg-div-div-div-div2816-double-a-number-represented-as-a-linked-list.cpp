79ec1bda27fe96a92033c9478d4896f63f10284c
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-05-07 16:23:02 +0530

    Time: 173 ms (63.84%), Space: 120.7 MB (72.53%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2816-double-a-number-represented-as-a-linked-list/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2816-double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* doubleIt(ListNode* head) {
+       ListNode* curr=head;
+        if((head->val)>=5){
+            ListNode* newnode=new ListNode(1);
+            newnode->next=head;
+            head=newnode;
+        }
+        while(curr!=NULL){
+           curr->val=(curr->val*2)%10+(curr->next && curr->next->val>=5);
+            curr=curr->next;
+        }
+        return head;
+    }
+};
