54cf8ec3eb402392c81bacf6096cd6f10c46525e
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-05-26 17:06:26 +0530

    Time: 1179 ms (29.19%), Space: 460.1 MB (13.66%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence14-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div552-student-attendance-record-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence14-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div552-student-attendance-record-ii.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence14-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div552-student-attendance-record-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence14-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div552-student-attendance-record-ii.cpp
@@ -3,28 +3,27 @@
 
 class Solution {
 public:
-    ll dp[100001][3][3];
     
-    ll solve(int n, int absentCount, int consecuLateCount) {
+    ll solve(int n, int absentCount, int consecuLateCount,vector<vector<vector<ll>>> &dp) {
         if (n == 0) return 1;
         if (dp[n][absentCount][consecuLateCount] != -1)
             return dp[n][absentCount][consecuLateCount];
         
         ll ans = 0;
         // P today
-        ans = (solve(n - 1, absentCount, 0)) % mod;
+        ans += (solve(n - 1, absentCount, 0,dp)) % mod;
         // A today, only 1 absent is allowed
         if (absentCount < 1)
-            ans += (solve(n - 1, absentCount + 1, 0)) % mod;
+            ans += (solve(n - 1, absentCount + 1, 0,dp)) % mod;
         // L today, only 2 days late allowed
         if (consecuLateCount < 2)
-            ans += ( solve(n - 1, absentCount, consecuLateCount + 1)) % mod;
+            ans += ( solve(n - 1, absentCount, consecuLateCount + 1,dp)) % mod;
         
         return dp[n][absentCount][consecuLateCount] = ans % mod;
     }
     
     int checkRecord(int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(n, 0, 0);
+      vector<vector<vector<ll>>>dp(n+1,vector<vector<ll>>(2,vector<ll>(3,-1)));
+        return solve(n, 0, 0,dp);
     }
 };
