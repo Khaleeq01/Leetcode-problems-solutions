b83df015be2ac5bc97c9efacbcb4402e4d5364b2
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-07-03 17:39:20 +0530

    Time: 90 ms (5.05%), Space: 38.9 MB (15.52%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -4,9 +4,29 @@ public:
         int n=nums.size();
         return min({nums[n-1]-nums[3],nums[n-2]-nums[2],nums[n-3]-nums[1],nums[n-4]-nums[0]});
     }
+    int minDiffViaPq(vector<int>& nums){
+        priority_queue<int>maxHeap;
+        priority_queue<int,vector<int>,greater<int>>minHeap;
+        for(auto el:nums){
+            maxHeap.push(el);
+            minHeap.push(el);
+            
+            if(maxHeap.size()>4) maxHeap.pop();
+            if(minHeap.size()>4) minHeap.pop();
+            
+        }
+        int l=3,r=nums.size()-4;
+        while(!maxHeap.empty()){
+            nums[l--]=maxHeap.top();
+            maxHeap.pop();
+            
+            nums[r++]=minHeap.top();
+            minHeap.pop();
+        }
+        return minDiff(nums);
+    }
     int minDifference(vector<int>& nums) {
         if(nums.size()<=4) return 0;
-        sort(nums.begin(),nums.end());
-        return minDiff(nums);
+        return minDiffViaPq(nums);
     }
 };
